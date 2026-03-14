024c8cc33b53b68915cd4e983a657c5b2b79f232
Kevin Sean Kho <kevinseankho@yahoo.com>
2026-03-14 10:15:00 +0800

    Chore(settings): add DEBUG import (#13)
    - We will only show /admin if the site is in DEBUG true.
    
    Co-authored-by: Kevin Sean Kho <kevin.kho@butterfly.com.au>

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,17 @@
 """
-    URL configuration for wingz project.
+    URL configuration for wingz-exam project.
 """
+
+from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from config.views import home
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/user/', include('apps.user.urls')),
     path('api/ride/', include('apps.ride.urls')),
 ]
 
 if settings.DEBUG:
-    from django.contrib import admin
     urlpatterns.append(path('admin/', admin.site.urls))
-
-    import debug_toolbar
-    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
